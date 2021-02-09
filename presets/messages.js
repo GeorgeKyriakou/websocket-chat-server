d2935bc18ab895104b980acef8b99ed7fa46791e
georgios.kyriakou@mirado.com <georgios.kyriakou@mirado.com>
2021-02-09 16:48:44 +0100

    Create chat function

---
+++ presets/messages.js
@@ -0,0 +1,7 @@
+const GREET_USER = "Welcome to the chat!";
+const USER_CONNECTED = "A user has joined your chat!";
+const USER_DISCONNECTED = "A user has left the chat";
+
+exports.GREET_USER = GREET_USER;
+exports.USER_CONNECTED = USER_CONNECTED;
+exports.USER_DISCONNECTED = USER_DISCONNECTED;
---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const router = express.Router();
+const http = require("http");
+const socketIO = require("socket.io");
+const presets = require("./presets/messages");
+const cors = require("cors");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.use(router);
+app.use(cors());
+app.options("*", cors());
+
+//Run when a client connects
+io.on("connect", (socket) => {
+  /** ************* Connect/Disconnect stuff ************* */
+
+  socket.broadcast.emit("msgFromServer", {
+    id: socket.id,
+    msg: presets.USER_CONNECTED,
+  }); //broadcast to everyone else
+
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("msgFromServer", {
+      id: null,
+      msg: presets.USER_DISCONNECTED,
+    });
+  });
+
+  /** ************* Handle messeging stuff ************* */
+  socket.on("msgFromUser", ({msg, username}) => {
+    io.emit("msgFromServer", { id: socket.id, msg, username }); //broadcast to everyone regardless
+  });
+});
+
+
+const PORT = 8080;
+server.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}` );
+});
